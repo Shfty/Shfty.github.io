1c9eebaa7ceb8d71f0bd46214e4add53768fd704
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2024-02-12 22:10:05 +0000

    Integrate hakyll-sass

--- posts/projects/academic/audio-visualizer.md
+++ posts/projects/academic/audio-visualizer.md
@@ -2,6 +2,7 @@
 title: Audio Visualizer
 git: shfty-csharp/AudioVisualizer
 published: 2024-02-08
+tags: Academic, C#
 todo: Screenshots, GIFs
 ---
 
--- site.hs
+++ site.hs
@@ -3,6 +3,7 @@
 
 import Data.Monoid (mappend)
 import Hakyll
+import Hakyll.Web.Sass (sassCompiler)
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -10,18 +11,19 @@ main = hakyll $ do
     let postPattern = "posts/**.md" .||. "posts/**.md.draft"
 
     let defaultTemplate = "templates/default.html"
-    let archiveTemplate = "templates/archive.html"
+    let projectsTemplate = "templates/projects.html"
     let postTemplate = "templates/post.html"
     let imageTemplate = "images/*" .||. "posts/**.png" .||. "posts/**.gif" .||. "posts/**.mkv"
-    let cssTemplate = "css/*"
+    let cssTemplate = "css/*.scss"
 
     match imageTemplate $ do
         route idRoute
         compile copyFileCompiler
 
-    match cssTemplate $ do
-        route idRoute
-        compile compressCssCompiler
+    scssDependency <- makePatternDependency cssTemplate
+    rulesExtraDependencies [scssDependency] $ match cssTemplate $ do
+        route $ setExtension "css"
+        compile (fmap compressCss <$> sassCompiler)
 
     match (fromList ["about.rst", "contact.markdown"]) $ do
         route $ setExtension "html"
@@ -38,18 +40,18 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate defaultTemplate postCtx
                 >>= relativizeUrls
 
-    create ["archive.html"] $ do
+    create ["projects.html"] $ do
         route idRoute
         compile $ do
             posts <- loadAll postPattern
-            let archiveCtx =
+            let projectsCtx =
                     listField "posts" postCtx (return posts)
-                        `mappend` constField "title" "Archives"
+                        `mappend` constField "title" "Projects"
                         `mappend` defaultContext
 
             makeItem ""
-                >>= loadAndApplyTemplate archiveTemplate archiveCtx
-                >>= loadAndApplyTemplate defaultTemplate archiveCtx
+                >>= loadAndApplyTemplate projectsTemplate projectsCtx
+                >>= loadAndApplyTemplate defaultTemplate projectsCtx
                 >>= relativizeUrls
 
     match "index.html" $ do
